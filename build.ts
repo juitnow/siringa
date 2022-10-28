3cd3c38585b2c3770ed1feb1d922c8f92a0418e7
Pier Fumagalli <pier@usrz.com>
2022-10-28 15:42:58 +0200

    Simplify build

--- build.ts
+++ build.ts
@@ -1,8 +1,5 @@
 import { tasks } from '@juitnow/build'
-import { build } from '@plugjs/plug'
 
-export default build({
-  ...tasks({
-    extraLintDir: 'test-d',
-  }),
+export default tasks({
+  extraLintDir: 'test-d',
 })
