c1fdda545555bef55e3bdff96a0f7e69aad7f49e
Pier Fumagalli <pier@usrz.com>
2023-02-27 19:51:10 +0100

    New build with `tsd` plugin

--- build.ts
+++ build.ts
@@ -1,5 +1,24 @@
-import { tasks } from '@juitnow/build'
+import { banner, build, find, hookAfter, tasks } from '@plugjs/build'
+import '@plugjs/tsd'
 
-export default tasks({
-  extraLintDir: 'test-d',
+const localBuild = build({
+  ...tasks(),
+
+  /** Run `tsd` */
+  async tsd(): Promise<void> {
+    banner('Testing type definitions')
+    await find('**/*.test-d.ts', { directory: 'test-d' }).tsd()
+  },
+
+  /** Lint all sources (including `test-d` stuff) */
+  async lint_tsd(): Promise<void> {
+    banner('Linting type defintion test sources')
+
+    await find('**/*.test-d.ts', { directory: 'test-d' }).eslint()
+  },
 })
+
+hookAfter(localBuild, 'test', [ 'tsd' ])
+hookAfter(localBuild, 'lint', [ 'lint_tsd' ])
+
+export default localBuild
